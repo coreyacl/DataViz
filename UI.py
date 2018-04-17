b486546c8f409002adc432167d6d958b193618d6
Corey <corey.cochranlepiz@gmail.com>
2018-04-17 13:04:56 -0400

    Started UI

--- UI.py
+++ UI.py
@@ -12,18 +12,54 @@ import pygame as py
 """ initiate pygame """
 py.init()
 clock = py.time.Clock()
-runnung = True
+running = True
 
 """ some important variables """
 display_width = 1600
+
 display_height = 1200
 gameDisplay = py.display.set_mode((display_width,display_height))
 py.display.set_caption("Occupation Viz")
 black = (0,0,0)
 white = (255,255,255)
 
+class GUI():
+    black = (0,0,0)
+    white = (255,255,255)
+    bx = 700
+    by = 100
 
-if running:
+    font = py.font.SysFont("couriernew",32)
+
+    def __init__(self,gD):
+        self.gD = gD
+
+    def update(self):
+        self.gD.fill(white)
+        box = py.surface.Surface((self.bx, self.by))
+        txt_surf = self.font.render("Data Viz!", False, self.white)  # bottom line
+        txt_rect = txt_surf.get_rect(center=(105, 40))
+        box.blit(txt_surf, txt_rect)
+        self.gD.blit(box,(30,0))
+
+
+
+gui = GUI(gameDisplay)
+
+while running:
+
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            running = False
+        if event.type == py.KEYUP:
+            if event.key == py.K_q:
+                running = False
+    # gameDisplay.fill(white)
+
+
+    gui.update()
+    py.display.update()
+    clock.tick(60)
     # do stuff...
 
 
