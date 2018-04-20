6b4792e42ab1cb5b5040557c62cf53581e979c7b
junwonlee5 <junwon.lee@students.olin.edu>
2018-04-20 11:19:27 -0400

    Graphs for visualization

--- graphs.py
+++ graphs.py
@@ -20,56 +20,56 @@ def create_worktime(free_time, work_time, sleep):
 def create_income(income):
     fig = plt.figure()
     ax = fig.add_subplot(111, aspect='equal')
-    income = income // 5000
+    income = income // 10000
     for i in range(0,income):
         x = [0.2,0.7,.8,.3]
         y = [0.15 + i*0.05,0.15+i*0.05,0.25+i*0.05,0.25+i*0.05]
-        ax.add_patch(patches.Polygon(xy=list(zip(x,y)), fill=False, color = 'green'))
+        ax.add_patch(patches.Polygon(xy=list(zip(x,y)), fill=True, edgecolor = (0, 0, 0), facecolor = 'green'))
         ax.set_xticks([])
         ax.set_yticks([])
-    ax.set_xlabel('Income (1 sheet = $5000)')
+    ax.set_xlabel('Income (1 sheet = $10000)')
     plt.show()
+    fig.savefig('income.png')
 
 def create_population(population):
-    image = Image.new('RGB', (200, 200))
+    pop = population //1000000
+    image = Image.new('RGB', (400, 400))
+    msg = 'Population Circle Diagram'
+    font = ImageFont.truetype('DejaVuSans.ttf', 15)
     draw = ImageDraw.Draw(image)
-    draw.ellipse((20, 20, 180, 180), fill = 'blue', outline ='blue')
-    draw.ellipse((70, 70, 110, 110), 'red')
+    draw.ellipse((20, 20, 380, 380), fill = 'blue', outline ='blue')
+    draw.text((0, 10), msg, (255, 255, 0), font = font)
+    #draw.text((100, 100), msg, (255, 255, 0), font = font)
+    #draw.text((10,10), "Hello world", font=font, fill=(255, 255, 0))
+    draw.ellipse((200 - 180 * pop //320 ,200 - 180 * pop //320,
+                200 + 180 * pop //320 , 200 + 180 * pop //320 ), 'red')
     image.show()
 
 def create_table():
-    states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
-            'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
-            'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
-            'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
-            'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
-    fig, ax = plt.subplots()
-    fig.patch.set_visible(False)
-    ax.axis('off')
-    ax.axis('tight')
-    df = pd.DataFrame(np.random.randn(1,len(states)), columns = states)
-    ax.table(cellText=df.values,colLabels=df.columns,loc = 'center')
-    plt.show()
+    vis = vincent.Map(width=1000, height=800)
+    #Add the US county data and a new line color
+    vis.geo_data(projection='albersUsa', scale=1000, counties=county_geo)
+    vis + ('2B4ECF', 'marks', 0, 'properties', 'enter', 'stroke', 'value')
+
+    #Add the state data, remove the fill, write Vega spec output to JSON
+    vis.geo_data(states=state_geo)
+    vis - ('fill', 'marks', 1, 'properties', 'enter')
+    vis.to_json(path)
 def create_happiness():
     fig, ax = plt.subplots()
-    index = np.arange(1)
-    bar_width = 0.15
+    index = np.arange(2)
+    bar_width = 0.3
     opacity = 0.8
-    rects1 = plt.bar(index+bar_width, 5, bar_width,
+    rects1 = plt.bar(index, [5,7], bar_width,
                  alpha=opacity,
-                 color='r',
-                 label='Averge Job')
-    rects2 = plt.bar(index + 2*bar_width, 7, bar_width,
-                 alpha=opacity,
-                 color='g',
-                 label='Particular Job')
+                 color='r')
     plt.ylabel('Happiness Value')
     plt.title('Happiness Factor')
-    plt.xticks(index + 3*bar_width, ('Average Job', 'Particular Job'))
-    plt.legend()
+    plt.xticks(index, ('Average Job', 'Particular Job'))
     plt.tight_layout()
     plt.show()
-#create_population(10)
-#create_income(65000)
-#create_happiness()
+    fig.savefig('Happiness_Factor.png')
+#create_population(65000000)
+create_income(65000)
+create_happiness()
 #create_table()
