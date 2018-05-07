913e5daf2440af3aa7b732898064d94d80f675e6
Corey <corey.cochranlepiz@gmail.com>
2018-05-07 16:21:27 -0400

    Tidy code and start using whiteboard

--- UI.py
+++ UI.py
@@ -6,6 +6,7 @@ Holds the user interface code for the program currently run off of Pygame.
 """
 
 import pygame as py
+import os.path
 
 
 
@@ -32,8 +33,7 @@ class Screen():
     by = 100
     font = py.font.SysFont("couriernew",25)
     title = py.font.SysFont("couriernew",50)
-    bg = py.image.load('mainFigures/Bb-2.jpg')
-    bg = py.transform.rotate(bg,270)
+    bg = py.image.load('FinalFigures/Wb-5.jpg')
     w,h = bg.get_size()
     bg = py.transform.scale(bg,(int(w*.7),int(h*.7)))
     listOfNames  = ['Farmer','Software Developer','Surgeon','Mechanical Engineer',
@@ -70,11 +70,18 @@ class Screen():
         the position can be modified with a new list that gets passed in the function
         just make sure that it's a list of tuples
         """
+        try:
+            if not os.path.exists(args[0]):
+                raise ValueError("Folder doesn't exist!")
+
         folder = args[0]
-        pars  = args[1] if len(args) > 1 else [(50,50,.9),(700,50,.9),(50,650,.9),(700,600,1)]
-        fileNames = ['income','time','mort','population']
-        for x in range(len(fileNames)):
-            self.addFigure(str(folder+fileNames[x]+'.png'),pars[x])
+        pars  = args[1] if len(args) > 1 else [(700,600,.9),(700,50,.9),(700,50,.9),(550,600,1),(50,50,1),(0,0,.05),(50,650,1)]
+        fileNames = ['income','time','mort','population','gender','race','map']
+        for x in range(len(pars)):
+            if os.path.isfile(folder+fileNames[x]+'.png'):
+                self.addFigure(str(folder+fileNames[x]+'.png'),pars[x])
+            else:
+                print(fileNames[x] + " not found for " + self.name)
 
     def addFigure(self,*args):
         if len(args) == 4:
@@ -94,6 +101,10 @@ class Screen():
         yl: y coordinate of figure
         scale: scaling factor of figure
         """
+        try:
+            if not os.path.exists(args[0]):
+                raise ValueError("Folder doesn't exist!")
+
         img = py.image.load(fig)
         w,h = img.get_size()
         img = py.transform.scale(img,(int(w*scale),int(h*scale)))
@@ -103,31 +114,30 @@ class Screen():
 
     def renderText(self,txt,pos,titleq=False):
         if titleq:
-            txt_surf = self.title.render(txt,False,self.white)
+            txt_surf = self.title.render(txt,False,self.black)
         else:
-            txt_surf = self.font.render(txt,False,self.white)
+            txt_surf = self.font.render(txt,False,self.black)
         txt_rect = txt_surf.get_rect(topleft=pos)
         self.gD.blit(txt_surf,txt_rect)
-
 
     def update(self,viewBackButton):
         """ Updates the screen with all necesarry details
         Does it IN ORDER so be careful
-
-
         """
         self.gD.fill(black)
         self.gD.blit(self.bg,(0,-600))
         if self.name == None:
+            # if Main Screen basically
             self.renderText("Occupation",(30,50),True)
             self.renderText("Visualization",(30,110),True)
             self.renderText("Made by:",(30,470))
             self.renderText("Alli Busa",(30,500))
             self.renderText("Corey Cochran-Lepiz",(30,530))
             self.renderText("Junwon Lee",(30,560))
-
             for x in range(len(self.listOfNames)):
                 self.renderText(self.listOfNames[x],(self.locs[x+1][0],self.locs[x+1][1]-30))
+        else:
+            self.renderText(self.name,(20,20),True)
         start = 0 if viewBackButton else 1
 
         for x in range(start,len(self.locs)):
@@ -139,7 +149,7 @@ mainScreen = Screen(gameDisplay,None)
 
 tlx,tly = 530,70
 sf = .3
-folder = 'FinalFigures'
+folder = 'FinalFigure'
 
 mainScreen.addFigure(folder+'/Farmer/rep.jpg',tlx,tly,.25)
 mainScreen.addFigure(folder+'/Software Developer/rep.jpg',tlx+530,tly,.27)
@@ -173,7 +183,7 @@ plumber = Screen(gameDisplay,'Plumber')
 plumber.addFromFolder(folder+'/Plumber/')
 
 # ADD IN ORDER!!
-screens = [mainScreen,farmer,phys,software,surgeon,meche,accountant,plumber]
+screens = [mainScreen,farmer,software,surgeon,meche,phys,plumber,accountant]
 
 screenIndex = 0
 while running:
